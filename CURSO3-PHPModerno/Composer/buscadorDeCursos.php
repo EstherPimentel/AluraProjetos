d087934dc129876d95a383e0357b188de6c1accc
EstherPimentel <85425371+EstherPimentel@users.noreply.github.com>
2022-02-16 02:06:50 -0300

    Curso 3 sobre PHP Composer

---
+++ CURSO3-PHPModerno/Composer/buscadorDeCursos.php
@@ -0,0 +1,28 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'src/Buscador.php';
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+$crawler = new Crawler();
+
+/*$resposta = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
+
+$html = $resposta->getBody();
+
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+
+$cursos = $crawler->filter('span.card-curso__nome');
+*/
+
+$buscador = new \Alura\BuscadorDeCursos\Buscador($client, $crawler);
+$cursos = $buscador->buscar('cursos-online-programacao/php');
+
+foreach($cursos as $curso) {
+    echo $curso . PHP_EOL;
+}
